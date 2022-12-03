e1d957be8dbcc631463d66e25177963e0a40a5a8
NeverMind109 <vladgladkih123@gmail.com>
2022-12-04 02:38:13 +0300

    Finished Channel Page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,10 +3,11 @@ import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
+
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: { xs: '356px', md: '320px' }, height: '326px', boxShadow: 'none', borderRadius: '20px', margin: 'auto' }}>
-      <Link to={`/channel/${channelDetail?.id?.ChannelId}`}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: { xs: '356px', md: '320px' }, height: '326px', boxShadow: 'none', borderRadius: '20px', margin: 'auto', marginTop }}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
           <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}/>
           <Typography variant="h6">
